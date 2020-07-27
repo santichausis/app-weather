2bb74865d34594fea3dc3a8b108a7f35bdce8f33
Santiago Chausis <santiagochausis@iMac-de-Santi.local>
2020-07-27 20:19:38 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import Clima from './components/Clima'
+import Error from './components/Error'
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
 
   const [consultar, guardarConsultar ] = useState(false)
   const [resultado, guardarResultado ] = useState({})
-
+  const [error, guardarError] = useState(false)
   const  { ciudad, pais } = busqueda;
 
   useEffect(() => {
@@ -28,10 +29,29 @@ function App() {
   
         guardarResultado(resultado)
         guardarConsultar(false)
+
+        // Detecta si hubo resultados correctos en la consulta
+
+        if(resultado.cod === "404") {
+          guardarError(true)
+        } else {
+          guardarError(false)
+        }
       }
     }
     consultarAPI()
+  // eslint-disable-next-line
   }, [consultar]);
+
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima
+    resultado={resultado}
+    />
+  }
+
 
   return (
     <Fragment>
@@ -46,9 +66,7 @@ function App() {
             ></Formulario>
           </div>
           <div className="col m6 s12">
-            <Clima
-              resultado={resultado}
-            />
+            {componente}
           </div>
         </div>
       </div>
